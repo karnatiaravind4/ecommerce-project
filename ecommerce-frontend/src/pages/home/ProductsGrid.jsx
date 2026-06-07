ab3cde2d005d344f45d8d7817463d7cccafac163
Aravind <karnatiaravind4@gmail.com>
2026-06-07 08:55:06 +0530

    first commit

---
+++ ecommerce-frontend/src/pages/home/ProductsGrid.jsx
@@ -0,0 +1,14 @@
+import {Product} from './Product';
+export function ProductsGrid({ products, loadCart }){
+  
+    return(
+              <div className="products-grid">
+                {products.map((product) => {
+                  return (
+                    <Product product={product} loadCart={loadCart} />
+                  );
+                })}
+          
+              </div>
+    );
+}
