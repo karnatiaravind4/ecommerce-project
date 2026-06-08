39bac0146d0d9e389f4c21dcf7bb858ee18366d1
Aravind <karnatiaravind4@gmail.com>
2026-06-08 23:18:35 +0530

    Add Vercel rewrites

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000'
+        target:'https://ecommerce-project-3-cb48.onrender.com'
       },
       '/images':{
-      target:'http://localhost:3000'
+      target:'https://ecommerce-project-3-cb48.onrender.com'
     }
   } }
 })
